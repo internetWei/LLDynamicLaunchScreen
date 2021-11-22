554bb0ca8042cf1608f3dd71031b3c4d3f3b0ac7
internetwei <internetwei@foxmail.com>
2021-11-23 05:53:28 +0800

    docs(location): 修改注释信息

--- LLDynamicLaunchScreen/LLDynamicLaunchScreen.h
+++ LLDynamicLaunchScreen/LLDynamicLaunchScreen.h
@@ -22,58 +22,44 @@ typedef NS_ENUM(NSInteger, LLLaunchImageType) {
 
 @interface LLDynamicLaunchScreen : NSObject
 
-/**
- 深色启动图校验规则，实现它可以修改深色启动图的识别逻辑。
- 
- @discussion 默认情况下，LLDynamicLaunchScreen会获取启动图最右上角1×1像素点的RGB值来判断
- 该图片是不是深色系图片。当您的APP启动图右上角像素点不是明显的深色RGB值时框架可能会判断错误深色
- 图片而导致启动图的修改失败，如果不幸发生了上述情况，您可以实现此Block来修改它的校验规则。
- 
- 
- 我尝试过各种方法来希望能自动判断深色图片，包括介不限于将图片压缩成1×1像素单位，然后对其取色进行判断。
- 但是都失败了，最后尝试了很多种方案发现还是目前的方案比较稳妥，但是如果您的启动图右上角颜色比较深，那么
- 可能会判断错误，导致启动图的修改失败。这时您就需要实现此Block来适配(如果您有更好的想法可以在github上留言或者邮箱联系我)。
- 
- @warnning 请尽量在`application: didFinishLaunchingWithOptions:`方法返回前实现它，否则可能无效。
- */
+/// 深色启动图校验规则，实现它可以修改深色启动图的识别逻辑。
+/// @discussion 默认情况下，LLDynamicLaunchScreen会获取启动图右上角1×1像素点的RGB值来判断该图片是不是深色系图片。
+///             当您的APP启动图右上角像素点不是明显的深色RGB值时可能会判断错误从而导致修改启动图失败。
+///             当APP首次安装或更新后首次打开时，LLDynamicLaunchScreen 会遍历您本地的启动图并且将它们的名称和 LLLaunchImageType 一一对应，
+///             默认情况下，框架会获取启动图右上角1×1像素点的RGB值来判断该启动图是不是深色启动图。
+///             这有可能会误判，所以您可以实现 hasDarkImageBlock 通过自定义校验规则并返回 YES 告诉框架这是深色启动图。
+/// @note 请在 application: didFinishLaunchingWithOptions: 方法返回前实现 hasDarkImageBlock 的判断，否则会失效。
+///       我尝试过很多方法希望能自动判断深色图片，包括但不限于网上很多的深色图片识别方法，但是都不理想，
+///       如果您有更好的想法，欢迎给我提交Issues。
 @property (nonatomic, class, null_resettable) BOOL (^hasDarkImageBlock) (UIImage *image);
 
 
-/**
- 设置/获取系统启动图的备份路径
- 
- @warnning 请尽量在`application: didFinishLaunchingWithOptions:`方法返回前实现它，否则可能无效。
- 
- 如果已上线，建议不要再修改路径。如果一定要修改请将之前路径上的文件备份到新路径下，否则可能会
- 导致用户更新版本后丢失上次设置的启动图信息。
- */
+/// 设置或获取系统启动图的备份路径。
+/// @discussion 默认情况下，LLDynamicLaunchScreen 会将启动图备份到 Document 文件夹下，
+///             如果您想修改它的备份路径，请调用 setLaunchImageBackupPath: 告诉它。
+/// @note 请在 application: didFinishLaunchingWithOptions:  方法返回前实现它，否则会失效。
+///       如果您的APP已经有用户安装了，请您在修改路径前先将原备份的内容拷贝到新路径，
+///       否则可能会导致用户更新版本后丢失上次设置的启动图信息。
 @property (nonatomic, class, null_resettable) NSString *launchImageBackupPath;
 
 
-/**
- 设置/获取替换启动图的备份路径
- 
- @warnning 请尽量在`application: didFinishLaunchingWithOptions:`方法返回前实现它，否则可能无效。
- 
- 如果已上线，建议不要再修改路径。如果一定要修改请将之前路径上的文件备份到新路径下，否则可能会
- 导致用户更新版本后丢失上次设置的启动图信息。
- */
+/// 设置或获取替换启动图的备份路径。
+/// @discussion 该目录保存用户选择的启动图，作用和注意事项请参考 launchImageBackupPath。
 @property (nonatomic, class, null_resettable) NSString *replaceLaunchImageBackupPath;
 
 
-/// 获取指定模式下APP的启动图(获取不到会返回nil)
+/// 获取指定模式下APP的启动图，如果获取不到会返回nil。
 + (nullable UIImage *)launchImageFromType:(LLLaunchImageType)launchImageType;
 
 
-/// 将所有启动图恢复为默认启动图
+/// 将所有启动图恢复为默认启动图。
 + (void)restoreAsBefore;
 
 
 /// 替换APP指定启动图
-/// @param replaceImage 需要写入的图片，nil表示恢复为默认启动图
-/// @param launchImageType _
-/// @param quality 图片压缩比例，默认为0.8
-/// @param validationBlock 自定义校验回调，返回YES表示替换，NO表示不替换，默认返回YES
+/// @param replaceImage 需要写入的图片，nil表示恢复为默认启动图。
+/// @param quality 图片压缩比例，默认为0.8。
+/// @param validationBlock 自定义校验回调，返回YES表示替换，NO表示不替换，默认返回YES。
 + (BOOL)replaceLaunchImage:(nullable UIImage *)replaceImage
            launchImageType:(LLLaunchImageType)launchImageType
         compressionQuality:(CGFloat)quality
